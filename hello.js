4f8e0bff16dd835d4164dca430e454779bd359ef
joinalzx <joinalzx@gmail.com>
2021-01-31 21:35:39 +0600

    lesson four done

--- hello.js
+++ hello.js
@@ -1,2 +1,22 @@
-var bananaPrice = 12;
-console.log(bananaPrice);
+// var bananaPrice = 12;
+// console.log(bananaPrice);
+
+// var names = "Smartly Coding"
+// console.log(typeof names);
+
+// var isHot = true;
+// console.log(isHot);
+// var isRich = false;
+
+
+var nam = 'Joinal Abden'
+console.log(nam);
+
+var $kr = 'Keyword Research and Link Building'; 
+console.log($kr);
+
+var myTime = "Start your day with the name of Allah"
+console.log(myTime);
+
+var userNameValue = "write the meaning full name when writing variable name"
+console.log(userNameValue.toUpperCase() );
